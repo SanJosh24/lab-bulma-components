3a16172f6c4bf796aa858d2a4666d9c613967738
Josh Matos <jmatos.5@aol.com>
2018-09-24 16:27:07 -0400

    iteration 1 complete

---
+++ src/component/CoolButton.js
@@ -0,0 +1,13 @@
+// User.js
+import React from 'react';
+
+const CoolButton = () => {
+	return (
+		<div>
+			<button class="button is-rounded my-class is-danger is-small">Login</button>
+			<button class="button is-small is-success">Signup</button>
+		</div>
+	);
+};
+
+export default CoolButton;
